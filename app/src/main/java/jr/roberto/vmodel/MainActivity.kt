8fc8177f74bbf62db19e04add2aaeb7f78633764
Roberto Júnior <rbezerra18@hotmail.com>
2021-03-19 15:50:11 -0300

    Implementar regras de negócio.

--- app/src/main/java/jr/roberto/vmodel/MainActivity.kt
+++ app/src/main/java/jr/roberto/vmodel/MainActivity.kt
@@ -1,11 +1,57 @@
 package jr.roberto.vmodel
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    
+    lateinit var txtContador: EditText
+    lateinit var btnDados: Button
+    lateinit var btnMostrar: Button
+
+    var contador: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initDados()
+        initContador()
+        initClick()
+
+        validaContador()
     }
+
+    private fun validaContador() {
+        if(contador > 5) {
+            contador = 0
+        }
+    }
+
+    private fun initDados() {
+        txtContador = findViewById(R.id.txtContador)
+        btnDados = findViewById(R.id.btnDados)
+        btnMostrar = findViewById(R.id.btnMostrar)
+
+    }
+
+    private fun initContador() {
+        txtContador.setText(contador.toString())
+    }
+
+    private fun initClick() {
+        btnDados.setOnClickListener {
+            contador++
+            validaContador()
+            initContador()
+        }
+
+        btnMostrar.setOnClickListener {
+            Toast.makeText(this,"Valor contador: ${contador.toString()}",Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
