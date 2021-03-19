f3bd4b815b00de08b92c94fb493f1db19db15d70
Roberto Júnior <rbezerra18@hotmail.com>
2021-03-19 16:21:36 -0300

    Identificar etapas do ciclo de vida.

--- app/src/main/java/jr/roberto/vmodel/MainActivity.kt
+++ app/src/main/java/jr/roberto/vmodel/MainActivity.kt
@@ -1,6 +1,7 @@
 package jr.roberto.vmodel
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -18,11 +19,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        logar(valor = "onCreate")
+
         initDados()
         initContador()
         initClick()
 
         validaContador()
+    }
+
+    override fun onStart() {
+        logar(valor = "onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        logar(valor = "onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        logar(valor = "onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        logar(valor = "onStop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        logar(valor = "onDestroy")
+        super.onDestroy()
+    }
+
+    private fun logar(tag: String = "Ciclo de vida", valor: String) {
+        Log.d(tag, valor)
     }
 
     private fun validaContador() {
